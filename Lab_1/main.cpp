f3eb80ade168075c921b98b7b8bbc66123311ef2
Forkkinstone <shenkoto5@gmail.com>
2024-09-23 13:58:41 +0200

    Add: Lab_1

---
+++ Lab_1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "char: 8 bit [-128, 127] or [0, 255]" << endl; // Вывод строки в консоль (cout) и переход на новую строку (endl)
+    cout << "short: 16 bit [-32768, 32767]" << endl;
+    cout << "int: 16 or 32 bit [-32768, 32767] or [-2147483648, 2147483647]" << endl;
+    cout << "unsigned int: 16 or 32 bit [0, 65535] or [0, 4294967295]" << endl;
+    cout << "long: 32 or 64 bit [-2147483648, 2147483647] or [-9223372036854775, 9223372036854774]" << endl;
+    cout << "long long: 64 bit [−9223372036854775808, 9223372036854775807]" << endl;
+    cout << "bool: True or False" << endl;
+    cout << "float: 32 bit [+/- 3.4E-38, 3.4E+38]" << endl;
+    cout << "double: 64 bit [+/- 1.7E-308, 1.7E+308]" << endl;
+    cout << "65 + 97 = " << 65+97 << endl; // Вывод в консоль строки и ответа
+    cout << "89 * 0.5 = " << 89*0.5 << endl;
+    cout << "976 : 6 = " << 976/6 << endl;
+    cout << "3*4 = " << 3*4 << endl; 
+    return 0;
+}
