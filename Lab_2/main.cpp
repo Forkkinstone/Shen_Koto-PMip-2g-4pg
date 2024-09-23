61fbad29297bd647fcbbf740764d34420def4282
Forkkinstone <shenkoto5@gmail.com>
2024-09-23 13:59:47 +0200

    Add: Lab_2

---
+++ Lab_2/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream> // Модуль ввода-вывода
+#include <cmath> // Библиотека матопераций 
+using namespace std; // Позволяет не писать std::
+int main()
+{
+    float x, n; // Объявляем переменные с типом float (тип с плавающей точкой)
+    cout << "Enter the first number -- "; // Выводим в консоль "Введите первое число -- "
+    cin >> x; // Пользователь вводит число 
+
+    if (x>0 && x<100) 
+    {
+    cout << "Enter the second number -- "; // Выводим в консоль "Введите второе число --"
+    cin >> n; // Пользователь вводит число
+    if (n>0 && n<100) 
+    {
+       cout << "Calculating the result..." << endl; // Вычисление результата, строка переходит на новую
+       cout << "Result = " << pow(x, (1/n)); // Результат 
+    }
+    else
+    {
+        cout << "You have entered a value that exceeds the allowed values."; // Если второе значение не удовлетворяет условию, выводится текст с ошибкой
+    }
+    }
+    else {
+        cout << "You have entered a value that exceeds the allowed values."; // Если первое значение не удовлетворяет условию, выводится текст с ошибкой
+    }
+
+    return 0; 
+
+}
