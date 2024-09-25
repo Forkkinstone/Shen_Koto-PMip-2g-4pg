609ec6a8695f816947a88df012c72f70ae691609
Forkkinstone <shenkoto5@gmail.com>
2024-09-25 12:45:24 +0200

    Add: Комментарии

--- Lab_5/main.cpp
+++ Lab_5/main.cpp
@@ -5,20 +5,20 @@
 int main ()
 {
     std::list <int> List = {2415, 84563, 925178, 9832, 98356, 4578, 8194, 914276, 734251, 893561, 845362, 916743823, 8935271, 12341, 39241764, 62372};
-    int sum = 0;
+    int sum = 0; // Изначально сумма равна нулю 
 
-    for (auto v: List) 
+    for (auto v: List) // Перебор значений из списка = v
     {
-        if (v % 10 != 1)
+        if (v % 10 != 1) // Условие 
         {
             std::cout << v << "\n";
             sum += v;
-            std::min_element(List.begin(), List.end());
+            
 
         }
 
     }
-    std::cout << sum << "!\n";
+    std::cout << sum << "!\n"; // Вывод суммы
 
     return 0;
 
