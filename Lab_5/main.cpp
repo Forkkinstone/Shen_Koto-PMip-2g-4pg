7d1639f036c338b7e273d5a6f14679d6ce44be06
Forkkinstone <shenkoto5@gmail.com>
2024-09-25 12:43:20 +0200

    Add:
    Вывод списка и сумма

--- Lab_5/main.cpp
+++ Lab_5/main.cpp
@@ -1,46 +1,27 @@
 #include <iostream>
-#include <vector>
-using namespace std; 
+#include <list>
+#include <algorithm>
 
-int main()
+int main ()
 {
-    int i, min, pos, sum, n;
-    cin >> n;
-    if (n>0)
-    {
-    vector <int> a(n);
-    for (i = 1; i < n; i++)
-    {
-        if (a[i]%10==1)
-        {
-            pos = i;
-            min = a[i];
-            sum = min;
-            break;
-        }
-        if (pos == -1)
-        {
-            cout << "Not found any number!\n";
-            return 0;
-        }
-    }
+    std::list <int> List = {2415, 84563, 925178, 9832, 98356, 4578, 8194, 914276, 734251, 893561, 845362, 916743823, 8935271, 12341, 39241764, 62372};
+    int sum = 0;
 
-    for (i=pos+1; i<n; i++)
+    for (auto v: List) 
     {
-        if (a[i]%10==0)
+        if (v % 10 != 1)
         {
-            sum += a[i];
-            if (a[i]<min)
-            {
-                min=a[i];
-                pos = i;
-            }
+            std::cout << v << "\n";
+            sum += v;
+            std::min_element(List.begin(), List.end());
+
         }
 
-
     }
+    std::cout << sum << "!\n";
 
-    }
+    return 0;
+
 
 
 
