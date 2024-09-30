096fa9197d2c6f7a3bb0738db6556d858b63fa2f
Forkkinstone <shenkoto5@gmail.com>
2024-09-30 19:07:11 +0200

    Add: Лабораторная №5.1

--- Lab_5/main.cpp
+++ Lab_5/main.cpp
@@ -4,14 +4,27 @@ int main ()
     int N;
     int i;
     int sum = 0;
-    const int i_min;
+    int i_min;
     std::cin >> N >> i_min;
+
     for (i=i_min; i<=N; i++) 
-    if (i%10!=1)
-    {
-        sum += i;
-    }
-    std::cout << sum << std::endl;
+    
+        if (i%10!=1)
+        {
+            sum += i;
+        }
+
+    std::cout << "sum: " << sum << std::endl;
+
+    for (i=i_min; i<=N; i++) 
+
+        if (i%10==1)
+        {
+            std::cout << "min_1: " << i << std::endl;
+            std::cout << "num_1: " << i-i_min + 1 << std::endl;
+            break;
+        }
+    
     return 0;
     
 
