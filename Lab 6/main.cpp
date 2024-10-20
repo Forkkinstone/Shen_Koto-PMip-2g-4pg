f0adf9af3fed394a5e42f4e3ab60fa0c5e2b3530
Forkkinstone <shenkoto5@gmail.com>
2024-10-20 15:58:23 +0200

    Add: 6.1 and so so 6.2

--- Lab 6/main.cpp
+++ Lab 6/main.cpp
@@ -2,6 +2,7 @@
 
 int main ()
 {
+    //1
     const int N_Max = 10000;
     int mas[N_Max];
 
@@ -23,8 +24,9 @@ int main ()
 
         while(value > 0)
             {
-                value = value / 10;
-                sum += value;
+                int num = value % 10;
+                value /= 10;
+                sum += num;
             } 
         
         if(sum == 12)
@@ -52,8 +54,70 @@ int main ()
     //Вывод
     for(int i=0; i<n; i++)
         std::cout << mas[i] << " ";
+
+
+
+
+
+
+
+
+    //2
+    // Ввод
+    for(int i=0; i<n; i++)
+        std::cin >> mas[i];
+
+
+
+    for(int i=0; i<n-1;i++)
+        for(int j=i+1; j<n; j++)
+            {
+                int value_I = mas[i];
+                int multiply_I = 0;
+
+                while(value_I > 0)
+                    {
+                        int num_I = value_I % 10;
+                        value_I /= 10;
+                        multiply_I *= value_I;
+                    }
+                
+                int value_J = mas[i];
+                int multiply_J = 0;
+
+                while(value_J > 0)
+                    {
+                        int num_J = value_J % 10;
+                        value_J /= 10;
+                        multiply_J *= value_J;
+                    }
+
+                if(multiply_I > multiply_J)
+                {
+                int tmp = mas[i];
+                mas[i] = mas[j];
+                mas[j] = tmp;
+                }
+            }
+
+    
+
+
+    //Вывод
+    for(int i=0; i<n; i++)
+        std::cout << mas[i] << " ";
+
+
+
+
+    
+
         
     return 0;
+
+
+
+
 
 }
 
