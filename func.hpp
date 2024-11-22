089b82c269db2eb89e5e478fa22691967da8845d
Forkkinstone <shenkoto5@gmail.com>
2024-11-22 22:23:17 +0200

    Add: Пожиральщик мусора

--- func.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace func
-{
-    void write(int matrix[100][100], int& i, int& j, int& n, int& m);
-    void read(int matrix[100][100], int& i, int& j, int& n, int& m);
-    bool isPrime(int num);
-}
