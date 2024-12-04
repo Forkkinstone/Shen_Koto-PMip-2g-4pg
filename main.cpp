e979cb7dc319510ae63e9fc021ad09f7102af96e
Forkkinstone <shenkoto5@gmail.com>
2024-12-04 11:41:52 +0200

    Add: Lab 8_2!

--- main.cpp
+++ main.cpp
@@ -2,143 +2,140 @@
 #include <string>
 #include <fstream>
 
-bool Read(int& n, std::string words[5120])
-{
+const int MAX_WORDS = 5120;
+
+bool Read(int& n, int& cnt, std::string words[MAX_WORDS]) {
     std::ifstream in("input.txt");
-    if(!in.is_open())
-    {
+    if (!in.is_open()) {
         std::cerr << "File not opened" << std::endl;
         return false;
     }
-
+    
     n = 0;
-    while(!in.eof())
-    {
-        in >> words[n];
+    while (in >> words[n] && n < MAX_WORDS) {
         n++;
     }
 
     return true;
 }
 
+void DebugWrite(std::string& word)
+{
+    std::string newWord;
+    for (char c : word)
+    {
+        if(!std::ispunct(c))
+            newWord += c;
+    }
+    word = newWord;
+}
+
+bool isVowel(char c) {
+    c = tolower(c);
+    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y');
+}
+
+bool hasDuplicateVowels(const std::string& word) {
+    int vowelCount[26] = {0};
+    for (char c : word) {
+        if (isVowel(c)) {
+            vowelCount[tolower(c) - 'a']++;
+            if (vowelCount[tolower(c) - 'a'] >= 2) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+void RemoveConsonants(int n, std::string words[MAX_WORDS]) {
+    for (int i = 0; i < n; i++) {
+        std::string newWord;
+        for (char c : words[i]) {
+            if (isVowel(c) || c == ' ' || c == ',') { // Assuming we want to keep spaces and commas
+                newWord += c;
+            }
+        }
+        words[i] = newWord;
+    }
+}
+
+void DuplicateVowels(std::string& word) {
+    std::string newWord;
+    for (char c : word) {
+        newWord += c;
+        if (isVowel(c)) {
+            newWord += c; // Duplicate vowel
+        }
+    }
+    word = newWord;
+}
+
+void Sort(int n, std::string words[MAX_WORDS]) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (words[i] > words[j]) {
+                std::swap(words[i], words[j]);
+            }
+        }
+    }
+}
+
+bool isNotLessThan3Vowels(const std::string& word) {
+    int count = 0;
+    for (char c : word) {
+        if (isVowel(c)) {
+            count++;
+        }
+    }
+    return count >= 3;
+}
+
 void Write(int& n, std::string words[5120])
 {
-    for(int i=0; i<n; i++)
-        std::cout << "<" << words[i] << ">" << std::endl;
-}
-
-std::string toLowerCase(std::string s)
-{
-    for(int i=0; i<s.length(); i++)
-    {
-        if('A' <= s[i] && s[i] <= 'Z')
-            s[i] += 32;
-    }
-    return s;
-}
-
-char toLower(char c)
-{
-    if('A' <= c && c <= 'Z')
-        c += 32;
-    return c;
-}
-
-bool isVowel(char c)
-{
-    c = toLower(c);
-    std::string vowels = "aeiouy";
-    if(vowels.find(toLower(c)) != std::string::npos)
-        return true;
-    return false;
-     
-}
-
-bool isWordWithEqualVowels(int& n, std::string words[5120])
-{
-    for(int i=0; i<n; i++)
-    {
-        std::string vowels = "aeiouy";
-        for(int j=0; j<vowels.length(); j++)
-        {
-            int cnt = 0;
-            for(int k=0; k<words[i].length(); k++)
-                if(toLower(words[i][k]) == vowels[j])
-                    cnt++;
-            if(cnt >= 2)
-                return true;
-
+    for (int i = 0; i < n; i++) {
+        if (!words[i].empty()) {
+            DebugWrite(words[i]);
+            std::cout << "<" << words[i] << ">" << std::endl;
         }
     }
-    return false;
 }
 
-void RemoveConsonant(int n, std::string words[5120])
-{
-    for(int i=0; i<n; i++)
-        {
-            for(int j=0; j<words[i].length(); j++)
-                if(!isVowel(words[i][j]))
-                {
-                    words[i].erase(j, 1);
-                    j--;
-                }
-        }
-}
+int main() {
 
-bool isNotLess3Vowels(const std::string& s)
-{
-    int cnt = 0;
-    for(int i=0; i<s.length(); i++)
-        if(isVowel(s[i]))
-            cnt++;
-    return cnt >= 3;
-}
-
-void DuplicateVowels(std::string& s)
-{
-    for(int i=0; i<s.length(); i++)
-        if(isVowel(s[i]))
-        {
-            s.insert(i, 1, s[i]);
-            i++;
-        }
-}
-
-void Sort(int n, std::string words[5120])
-{
-    for(int i=0; i<n-1; i++)
-        for(int j=i+1; j<n; j++)
-            if(toLowerCase(words[i]) < toLowerCase(words[j]))
-                std::swap(words[i], words[j]);
-
-}
-
-int main()
-{
     int n;
-    std::string words[5120];
+    int cnt;
+    int countLetters[MAX_WORDS] = {0};
+    std::string words[MAX_WORDS];
 
-    Read(n, words);
+    if (!Read(n, cnt, words)) {
+        return -1;
+    }
 
-    if(isWordWithEqualVowels(n, words))
-        RemoveConsonant(n, words);
-    else 
-    {
-        for(int i = 0; i<n; i++)
-            if(isNotLess3Vowels(words[i]))
+    bool hasEqualVowels = false;
+    for (int i = 0; i < n; i++) {
+        if (hasDuplicateVowels(words[i])) {
+            hasEqualVowels = true;
+            break;
+        }
+    }
+
+    if (hasEqualVowels) {
+        RemoveConsonants(n, words);
+    } else {
+        for (int i = 0; i < n; i++) {
+            if (isNotLessThan3Vowels(words[i])) {
                 DuplicateVowels(words[i]);
-
+            }
+        }
     }
 
     Sort(n, words);
 
     Write(n, words);
+
     
 
     return 0;
-
-
-
-
 }
+
